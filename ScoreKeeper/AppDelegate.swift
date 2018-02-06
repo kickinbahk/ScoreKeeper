e2ca994cc07ab20648d85dc753305362593e69c5
kickinbahk@gmail.com <kickinbahk@gmail.com>
2018-02-06 14:03:51 -0800

    Initial Commit

---
+++ ScoreKeeper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScoreKeeper
+//
+//  Created by Josiah Mory on 2/6/18.
+//  Copyright © 2018 kickinbahk Productions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScoreKeeper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScoreKeeper
+//
+//  Created by Josiah Mory on 2/6/18.
+//  Copyright © 2018 kickinbahk Productions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
